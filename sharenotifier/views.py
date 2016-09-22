d682d200731e5d4cf119fb54b9fb8a672c5d495a
rmagesh148 <rmagesh148@gmail.com>
2016-09-21 21:38:01 -0500

    Added image, validated unique_together, validated POST form

--- sharenotifier/views.py
+++ sharenotifier/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from allauth.socialaccount.models import SocialAccount
 from sharenotifier.forms import AddForm
 from sharenotifier.models import StockDetails as sd
+from django import forms
 
 # Create your views here.
 
@@ -17,16 +18,39 @@ def homepage(request):
     if request.method == 'GET':
         add_form = AddForm()
         user = User.objects.get(username=request.user)
-        print user.id,"udwe id"
+        #print user.id,"udwe id"
         extract_data = SocialAccount.objects.get(user_id=user.id)
         email = extract_data.extra_data['email']
+        img_src = extract_data.extra_data['picture']
         first_name = user.first_name
         stock_values = sd.objects.all()
         if stock_values:
-            return render(request,'homepage.html', {'first_name':first_name, 'form': add_form, 'stock_values':stock_values})
+            return render(request,'homepage.html', {'first_name':first_name, 'form': add_form, 'stock_values':stock_values, 'img_src': img_src})
         else:
             return render(request, 'homepage.html',
-                          {'first_name': first_name, 'form': add_form, "message": "OOPS! You've no items to show up."})
+                          {'first_name': first_name, 'form': add_form, "message": "OOPS! You've no items to show up.", 'img_src': img_src})
+    if request.method == "POST":
+        form = AddForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(username=request.user)
+            company_code = form.cleaned_data['company_code']
+            target_price = form.cleaned_data['target_price']
+            extract_data = SocialAccount.objects.get(user_id=user.id)
+            email = extract_data.extra_data['email']
+            img_src = extract_data.extra_data['picture']
+            first_name = user.first_name
+            stock_values = sd.objects.all()
+            if sd.objects.filter(company_code = company_code,user_id = user.id).exists():
+                return render(request, 'homepage.html',
+                              {'first_name': first_name,'form':form, 'stock_values': stock_values,
+                               'img_src': img_src, 'info_message_red': 'Same Company code!'})
+            else:
+                form = AddForm()
+                sd.objects.create(company_code=company_code,target_price=target_price,user_id=user.id)
+                return render(request, 'homepage.html',
+                              {'first_name': first_name, 'form': form, 'stock_values': stock_values,
+                               'img_src': img_src, 'info_message_green': 'Company code and Price added!'})
+
     return render(request, 'homepage.html')
 
 
